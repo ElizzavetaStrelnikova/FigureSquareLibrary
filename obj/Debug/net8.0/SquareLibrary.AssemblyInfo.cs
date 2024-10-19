5a63e884a5775cbfc1f65bfdaa23f84fc6fc5b1a
Обезличенная УЗ для стажировки-направление frontend-разработ. 12 <cr_ovr_fr12@efko.ru>
2024-10-19 16:50:04 +0300

    add tests

--- obj/Debug/net8.0/SquareLibrary.AssemblyInfo.cs
+++ obj/Debug/net8.0/SquareLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SquareLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8209eb4307cf358c13dcc45599fc94a1b5e3120")]
 [assembly: System.Reflection.AssemblyProductAttribute("SquareLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SquareLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
